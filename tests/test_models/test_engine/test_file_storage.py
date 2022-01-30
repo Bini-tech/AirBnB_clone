4b60fe9dc8d7ce743ee5b28745fc1911ede4624e
root <biniyamgetaneh30@gmail.com>
2022-01-30 10:33:37 -0800

    tests file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Unittest module for the FileStorage class."""
 
-from imports import *
 import unittest
 from datetime import datetime
 import time
@@ -41,14 +40,6 @@ class TestFileStorage(unittest.TestCase):
         with self.assertRaises(TypeError) as e:
             FileStorage.__init__()
         msg = "descriptor '__init__' of 'object' object needs an argument"
-        self.assertEqual(str(e.exception), msg)
-
-    def test_3_init_many_args(self):
-        """Tests __init__ with many arguments."""
-        self.resetStorage()
-        with self.assertRaises(TypeError) as e:
-            b = FileStorage(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-        msg = "object() takes no parameters"
         self.assertEqual(str(e.exception), msg)
 
     def test_5_attributes(self):
